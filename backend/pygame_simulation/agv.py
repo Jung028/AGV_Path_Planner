f1277a8d2b6977146e74ab32d5a88a1a2bf0b8b4
Adam <aedamjung@gmail.com>
2025-03-20 09:48:46 +0800

    changed the simulation back to grid based

--- backend/pygame_simulation/agv.py
+++ backend/pygame_simulation/agv.py
@@ -1,6 +1,4 @@
-# agv.py
-
-from config import BLACK, GRID_SIZE
+from config import BLACK, RED, GRID_SIZE
 import pygame
 
 class AGV:
@@ -10,8 +8,10 @@ class AGV:
         self.selected = False
 
     def draw(self, screen):
-        pygame.draw.rect(screen, BLACK, (self.position[0] * GRID_SIZE, self.position[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
-    
+        color = RED if self.selected else BLACK  # Highlight AGV when selected
+        pygame.draw.rect(screen, color, 
+                         (self.position[0] * GRID_SIZE, self.position[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+
     def move(self):
         if self.target:
             if self.position[0] < self.target[0]:
@@ -22,5 +22,15 @@ class AGV:
                 self.position[1] += 1
             elif self.position[1] > self.target[1]:
                 self.position[1] -= 1
+            
             if self.position == self.target:
                 self.target = None
+
+    def handle_click(self, x, y):
+        """Handles click events to select AGV or set target position."""
+        grid_x, grid_y = x // GRID_SIZE, y // GRID_SIZE
+        if self.position == [grid_x, grid_y]:  # Clicked on AGV
+            self.selected = not self.selected
+        elif self.selected:  # Clicked on a destination while AGV is selected
+            self.target = [grid_x, grid_y]
+            self.selected = False  # Deselect AGV after selecting target
